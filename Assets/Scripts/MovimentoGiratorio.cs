4838cade054d43a528da7f6db5814ce9d09902b7
AlfaceOrtodoxo <natanbbarreira@gmail.com>
2024-08-24 17:49:09 -0300

    Criação de novas cenas e script de movimento com velocidade fixa

--- Assets/Scripts/MovimentoGiratorio.cs
+++ Assets/Scripts/MovimentoGiratorio.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MovimentoGiratorio : MonoBehaviour
 {
+    private Scene currentScene;
     public float radius = 5f; // Raio do movimento circular
     public float angularSpeed = 1f; // Velocidade angular (em radianos por segundo)
 
@@ -12,20 +16,57 @@ public class MovimentoGiratorio : MonoBehaviour
 
     public float cooldown = 0;
 
-    public bool troca = false;
+    public bool troca = true;
+
+     void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "Fase1" || currentScene.name == "Fase2" || currentScene.name == "Fase3" || currentScene.name == "Fase6" || currentScene.name == "Fase10")
+        {
+            troca = true;
+        }
+        else if (currentScene.name == "Fase4" || currentScene.name == "Fase5" || currentScene.name == "Fase7")
+        {
+            troca = !troca;
+        }
+
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Sorteio();
-        Movimento();
+        Debug.Log(currentScene.name);
+        if (currentScene.name == "Fase8" || currentScene.name == "Fase9" || currentScene.name == "FaseInfinita")
+        {
+            Sorteio();
+            MovimentoAcelerado();
+        }
+        else 
+        {
+            MovimentoSimples();
+        }
+        
     }
 
-    void Movimento()
+    void MovimentoAcelerado()
     {
         // Calcula o ângulo baseado no tempo e na velocidade angular
-        float direction = troca ? -1f : 1f; // Determina a direção com base no estado de isReversed
-        angle += direction * angularSpeed * Time.deltaTime;
+        float direction = troca ? -1f : 1f; // Determina a direção com base no estado de troca
+        angle += direction * angularSpeed * Time.deltaTime; //Operador += dá aceleração
+
+        // Cria um novo vetor para representar a rotação no eixo Z (ou qualquer outro eixo)
+        Vector3 rotationAxis = new Vector3(0, 0, 1); // Eixo Z como exemplo
+
+        // Aplica a rotação ao redor do eixo definido
+        transform.Rotate(rotationAxis, angle * direction);
+    }
+
+    void MovimentoSimples()
+    {
+        // Calcula o ângulo baseado no tempo e na velocidade angular
+        float direction = troca ? -1f : 1f; // Determina a direção com base no estado de troca
+        angle = direction * angularSpeed * radius;
 
         // Cria um novo vetor para representar a rotação no eixo Z (ou qualquer outro eixo)
         Vector3 rotationAxis = new Vector3(0, 0, 1); // Eixo Z como exemplo
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,21 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject BombPrefab;
     [SerializeField] private GameObject DunotPrefab;
+    [SerializeField] private GameObject EmpityPrefab;
     [SerializeField] private int MinRange;
     [SerializeField] private int MaxRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        var RandomSpawn = Random.Range(MinRange, MaxRange);
-        if (RandomSpawn == MinRange) 
+        var RandomSpawn = Random.Range(MinRange, MaxRange); 
+        if (RandomSpawn == 0) 
         {
             GameObject newObject = Instantiate(BombPrefab, this.transform.position, Quaternion.identity, this.transform);
+        }
+        else if (RandomSpawn == 1)
+        {
+            GameObject newObject = Instantiate(EmpityPrefab, this.transform.position, Quaternion.identity, this.transform);
         }
         else
         {
